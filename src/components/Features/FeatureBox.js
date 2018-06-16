8d9c5dbe0f8c7618b17665d6db076be1660017fb
Gleb Fedchenko <fedchenkogleb@gmail.com>
2018-06-16 17:14:29 +0300

    finished features section

--- src/components/Features/FeatureBox.js
+++ src/components/Features/FeatureBox.js
@@ -1,7 +1,23 @@
 import React from "react";
-
+import { featureData } from "./featureData";
 const FeatureBox = () => {
-  return <div />;
+  return (
+    <div>
+      {featureData.map((item, i) => {
+        return (
+          <div key={i} className="col-1-of-4">
+            <div className="feature-box">
+              <i className={`feature-box__icon ${item.icon}`} />
+              <h3 className="heading-tertiary u-margin-bottom-small">
+                {item.name}
+              </h3>
+              <p className="feature-box__text">{item.text}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default FeatureBox;
---
+++ src/components/Features/featureData.js
@@ -0,0 +1,34 @@
+export const featureData = [
+  {
+    icon: "fa fa-globe",
+    name: "Explore the world",
+    text: ` Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum
+    minus animi facere temporibus possimus, atque maiores,
+    necessitatibus suscipit dolores ea quis, consequatur ab. Ipsa
+    obcaecati odio cum repellat, iure quibusdam!`
+  },
+  {
+    icon: "far fa-compass",
+    name: "Meet nature",
+    text: ` Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum
+    minus animi facere temporibus possimus, atque maiores,
+    necessitatibus suscipit dolores ea quis, consequatur ab. Ipsa
+    obcaecati odio cum repellat, iure quibusdam!`
+  },
+  {
+    icon: "far fa-map",
+    name: "Find your way",
+    text: ` Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum
+    minus animi facere temporibus possimus, atque maiores,
+    necessitatibus suscipit dolores ea quis, consequatur ab. Ipsa
+    obcaecati odio cum repellat, iure quibusdam!`
+  },
+  {
+    icon: "fas fa-heartbeat",
+    name: "Live a healthier life",
+    text: ` Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum
+    minus animi facere temporibus possimus, atque maiores,
+    necessitatibus  dolores ea quis, consequatur ab. Ipsa
+    obcaecati odio cum repellat, iure quibusdam!`
+  }
+];
